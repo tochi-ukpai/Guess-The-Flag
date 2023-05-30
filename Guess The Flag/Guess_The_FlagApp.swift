e1f08c5325c0f8e655cd1250e7712253c20c36a0
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-05-30 07:56:16 +0100

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Theós on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
